1216c45462e0a3856ba28e31905af1b3cef13ab0
Aldana Casé <aldana_case23@hotmail.com>
2018-09-05 13:00:01 -0300

    Creacion GUI

--- src/GUI/gui.java
+++ src/GUI/gui.java
@@ -1,10 +1,63 @@
 package GUI;
 
-public class gui {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+import Juego.Juego;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+	public class gui extends JFrame {
+
+		private JPanel contentPane;
+		
+		private Juego game;
+
+		/**
+		 * Launch the application.
+		 */
+		public static void main(String[] args) {
+			EventQueue.invokeLater(new Runnable() {
+				public void run() {
+					try {
+						gui frame = new gui();
+						frame.setVisible(true);
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+				}
+			});
+		}
+
+		/**
+		 * Create the frame.
+		 */
+		public gui() {
+			addKeyListener(new KeyAdapter() {
+				@Override
+				public void keyReleased(KeyEvent arg0) {
+					mover(arg0);
+				}
+			});
+			getContentPane().setLayout(null);
+			
+			setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			setBounds(100, 100, 450, 300);
+			contentPane = new JPanel();
+			contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+			setContentPane(contentPane);
+			contentPane.setLayout(null);
+			
+			game = new Juego(this);//inicia el juego
+		}
+		
+		protected void mover(KeyEvent key){
+			game.mover(key.getKeyCode());
+			
+			this.repaint();
+		}
 	}
-
-}
