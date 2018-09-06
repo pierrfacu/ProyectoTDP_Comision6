9ad4963c7158d0320c982f718554ac11cbc2af7a
Pierr Facu <facu.pierr@gmail.com>
2018-09-05 21:44:40 -0300

    Corrección de nombre en clases

---
+++ src/Entidades/Jugador.java
@@ -0,0 +1,8 @@
+package Entidades;
+
+public class Jugador extends Entidad{
+
+	public Jugador() {
+		
+	}
+}
--- src/Juego/Juego.java
+++ src/Juego/Juego.java
@@ -1,7 +1,16 @@
 package Juego;
 
+import GUI.gui;
+import Entidades.*;
+
 public class Juego {
 
+	private Jugador jugador;
+	
+	
+	public Juego(gui gui) {
+		
+	}
 	
 	
 }
