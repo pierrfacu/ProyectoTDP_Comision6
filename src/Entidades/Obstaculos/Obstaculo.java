fcf489c9821032f927bbac33bd2133045379d58d
Pierr Facu <facu.pierr@gmail.com>
2018-09-26 11:18:49 -0300

    Agregados

---
+++ src/Entidades/Obstaculos/Obstaculo.java
@@ -0,0 +1,16 @@
+package Entidades.Obstaculos;
+
+import Entidades.Entidad;
+
+/**
+ * 
+ * @author 
+ *
+ */
+public abstract class Obstaculo extends Entidad{
+
+	protected Obstaculo(int x, int y) {
+		super(0, x, y);
+	}
+
+}
---
+++ src/Entidades/PowerUps/PowerUp.java
@@ -0,0 +1,16 @@
+package Entidades.PowerUps;
+
+import Entidades.Entidad;
+
+/**
+ * 
+ * @author
+ *
+ */
+public abstract class PowerUp extends Entidad{
+
+	protected PowerUp(int x, int y) {
+		super(5, x, y);
+	}
+
+}
