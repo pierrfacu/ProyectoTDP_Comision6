76bdd921a708a61b47c076a0e74cc48af37e0150
Heinrich Stefania <teefii4sep@gmail.com>
2018-10-24 22:09:52 -0300

    Implementacion metodo atacar clase Buscador
    Publico metodo setGrafico de clase Entidad

--- src/Inteligencia/Buscador.java
+++ src/Inteligencia/Buscador.java
@@ -2,6 +2,9 @@ package Inteligencia;
 
 import Entidades.Enemigos.Enemigo;
 import Juego.JuegoEnemigo;
+
+import java.awt.Point;
+
 import Entidades.Jugador;
 
 public class Buscador extends Inteligencia {	
@@ -13,9 +16,17 @@ public class Buscador extends Inteligencia {
 	//Metodos
 	
 	public void atacar() {
-		JuegoEnemigo je= JuegoEnemigo.getInstance();
-		Jugador j= JuegoEnemigo.obtenerJugador();
-		if(j.)
+		JuegoEnemigo juegoEnem = JuegoEnemigo.getInstance();
+		Point posJug = juegoEnem.obtenerJugador().getPosition();
+		Point posEnem=enemigo.getPosition();
+		Point posNueva=new Point(0,0);
+        if(posEnem.x < posJug.x && posEnem.y < posJug.y)
+        	posNueva.setLocation(posEnem.x + velocidad, posEnem.y + velocidad);
+        if(posEnem.x > posJug.x && posEnem.y < posJug.y)
+        	posNueva.setLocation(posEnem.x - velocidad, posEnem.y + velocidad);
+        if(posEnem.x == posJug.x && posEnem.y > posJug.x)
+        	posNueva.setLocation(posEnem.x, posEnem.y + velocidad);
+        enemigo.setPosition(posNueva);
+        enemigo.setGrafico(0);
 	}
-
 }
